a899bae28d95895a497fc6e056ab9e8eb48f2644
Janus_Wang <wjn5516@gmail.com>
2025-04-07 23:45:02 +0800

    add dark theme

--- samples/sample.rs
+++ samples/sample.rs
@@ -1,23 +1,31 @@
 #[derive(Debug)]
 struct HasLifetime<'a> {
-  some_string: &'a str
+    some_string: &'a str,
 }
 
 fn do_something<'a>(has_lifetime: &HasLifetime<'a>) {
-  println!("{}", has_lifetime.some_string);
+    println!("{}", has_lifetime.some_string);
 }
 
 fn main() {
-  let has_lifetime = HasLifetime {
-    some_string: "example"
-  };
-  do_something(&has_lifetime);
+    let has_lifetime = HasLifetime {
+        some_string: "example",
+    };
+    do_something(&has_lifetime);
 }
 
 type TimeoutFunction = Box<Fn(&str, &ArgsMatcher) -> ()>;
 
 fn whatever(&self, closure: Box<Fn() + 'static>) {
-  // do nothing
+    // do nothing
 }
 
 pub unsafe fn hello() {}
+
+fn main() {
+    let a = 1;
+    let mut b = 2;
+    let c = &a;
+    let d = &mut b;
+    let mut e = &mut b;
+}
